dc699c72dc30da412b990057c33ea52a1031e1e4
Davi Souza <davisouza2001dv@gmail.com>
2023-09-22 23:09:05 -0300

    eslint configured

--- src/app/login/components/PasswordInput.tsx
+++ src/app/login/components/PasswordInput.tsx
@@ -2,42 +2,43 @@
 import { useState } from 'react'
 import { UseFormRegisterReturn } from 'react-hook-form'
 import { Button, Input, useToast, Collapse, Text, Icon } from '@chakra-ui/react'
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 interface PasswordInputProps {
-    value: UseFormRegisterReturn<"password">
+  value: UseFormRegisterReturn<'password'>
 }
 
 export function PasswordInput(props: PasswordInputProps) {
-    const [showPassword, setShowPassword] = useState(false)
-    return (
-        <>
-            <Text mb='8px' className='text-black font-medium'>Senha</Text>
-            <div className='flex items-center justify-center'>
-                <Input
-                    placeholder="$%-#4&21-*!@"
-                    size='sm'
-                    backgroundColor={'gray.200'}
-                    borderRightRadius={0}
-                    height={'12'}
-                    type={showPassword ? 'text' : 'password'}
-                    {...props.value}
-                />
-                <Button
-                    height={'12'}
-                    size='sm'
-                    backgroundColor={'gray.200'}
-                    borderLeftRadius={0}
-                    onClick={() => setShowPassword(!showPassword)}
-                >
-                    {showPassword ? (
-                        <Icon as={AiOutlineEye} height={5} width={5} />
-                    ) : (
-                        <Icon as={AiOutlineEyeInvisible} height={5} width={5} />
-                    )}
-                </Button>
-            </div>
-        </>
-    )
+  const [showPassword, setShowPassword] = useState(false)
+  return (
+    <>
+      <Text mb="8px" className="text-black font-medium">
+        Senha
+      </Text>
+      <div className="flex items-center justify-center">
+        <Input
+          placeholder="$%-#4&21-*!@"
+          size="sm"
+          backgroundColor={'gray.200'}
+          borderRightRadius={0}
+          height={'12'}
+          type={showPassword ? 'text' : 'password'}
+          {...props.value}
+        />
+        <Button
+          height={'12'}
+          size="sm"
+          backgroundColor={'gray.200'}
+          borderLeftRadius={0}
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? (
+            <Icon as={AiOutlineEye} height={5} width={5} />
+          ) : (
+            <Icon as={AiOutlineEyeInvisible} height={5} width={5} />
+          )}
+        </Button>
+      </div>
+    </>
+  )
 }
