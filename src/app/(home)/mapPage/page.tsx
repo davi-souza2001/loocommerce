e176fa533edb74d89cc7b4560c71afaf60e99a63
Davi Souza <davisouza2001dv@gmail.com>
2023-09-26 15:02:14 -0300

    write url

--- src/app/(home)/mapPage/page.tsx
+++ src/app/(home)/mapPage/page.tsx
@@ -1,43 +1,70 @@
 'use client'
 import Head from 'next/head'
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
+import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { useEffect, useRef, useState } from 'react'
+import test from '../../../../geoJSON.json'
 
 export default function MapPage() {
-  const Map = ReactMapboxGl({
-    accessToken:
-      'pk.eyJ1Ijoic29uZXJvIiwiYSI6ImNsbjBmcHk0ODFudnQyanQzeHhjajVlajUifQ.OZKrFSp_zAH2O0FCt51-ng',
-  })
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_URL_MAPBOX ?? ''
+  const mapContainer = useRef('map')
+  const map = useRef<mapboxgl.Map | null>(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  // const Map = new mapboxgl.Map({
+  //   container: 'map', // container ID
+  //   style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  //   center: [-74.5, 40], // starting position [lng, lat]
+  //   zoom: 9, // starting zoom
+  // })
+
+  useEffect(() => {
+    if (map.current) return
+    const mapLoaded = (map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/light-v11',
+      center: [lng, lat],
+      zoom,
+    }))
+
+    mapLoaded.on('load', () => {
+      mapLoaded.addSource('earthquakes', {
+        type: 'geojson',
+        data: '../../../../geoJSON.json',
+      })
+
+      mapLoaded.addLayer({
+        id: 'maine',
+        type: 'fill',
+        source: 'maine', // reference the data source
+        layout: {},
+        paint: {
+          'fill-color': '#0080ff', // blue color fill
+          'fill-opacity': 0.5,
+        },
+      })
+    })
+  }, [])
 
   return (
     <main className="w-full flex z-0">
       <Head>
         <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css"
+          href="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css"
           rel="stylesheet"
         />
       </Head>
       <div className="w-28" />
-      <div className="w-full mt-16 bg-red-500">
+      <div className="h-[75vh] w-full mt-16">
         <h1 className="text-2xl font-semibold my-8 ml-10">
           Regiões de entrega
         </h1>
-        <div className="h-[30rem] w-full bg-red-500">
-          <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            containerStyle={{
-              height: '100%',
-              width: '100%',
-            }}
-          >
-            <Layer
-              type="symbol"
-              id="marker"
-              layout={{ 'icon-image': 'marker-15' }}
-            >
-              <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-            </Layer>
-          </Map>
+        <div className="h-full w-full">
+          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+          {/* @ts-ignore */}
+          <div ref={mapContainer} className="h-full rounded-md" />
         </div>
       </div>
       <div className="w-16" />
